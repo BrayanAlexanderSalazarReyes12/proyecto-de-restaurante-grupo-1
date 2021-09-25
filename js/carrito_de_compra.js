491c5c574fc204d2160c3a491de7d98217ed25d1
Luis Angel <Vasquez092020@outlook.es>
2021-09-25 16:37:11 -0500

    Optimizando js

--- js/carrito_de_compra.js
+++ js/carrito_de_compra.js
@@ -4,10 +4,55 @@
  * @version 	0.1
  */
 
-var modal = document.getElementById('exampleModalToggle');
+var modal = document.querySelector('.icon-carrito');
+const list_prod = document.querySelector("#productos")
+const productos = ['Nombre 1','Nombre 2','Nombre 3','Nombre 4','Nombre 5','Nombre 6'];
+var listaP = '';
 
-function carrito(){
-    modal.innerHTML ="<div class='modal-dialog modal-fullscreen'>"+
+listar_productos = (productos) => {
+    for (let index = 0; index < productos.length; index++) {
+        const producto = productos[index];
+        
+        listaP +=
+        `
+        <div class="producto">
+            <img class="col-5" src="img/carrito/img1.jpg" alt="">
+            <div class="descripcion col-7 position-relative">
+                <div class="d-flex">
+                    <strong class="col-6 titulo col-4">${producto}</strong>
+                    <strong class="col-6 precio text-end">$25,000</strong>
+                </div>
+                <p class="text-break text-wrap">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae consequuntur placeat labore inventore at enim doloribus rem, quasi, dolorem iste sit veritatis quibusdam beatae laboriosam in itaque! Ducimus, perferendis doloribus.</p>
+                <div class="w-100 d-flex justify-content-around position-absolute" style="bottom: 10px;">
+                    <div class="add_delet">
+                        <small>-</small>
+                        <small>1</small>
+                        <small>+</small>
+                    </div>
+                    <div class="iconos">
+                        <i class="far fa-trash-alt"></i>
+                        <i class="fas fa-plus"></i>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+        `;
+    }
+
+    list_prod.innerHTML = listaP;
+}
+
+
+
+modal.addEventListener("click", () => {
+    listar_productos(productos)
+})
+
+
+/* function carrito(){
+    modal.innerHTML = "<div class='modal-dialog modal-fullscreen'>"+
             "<div class='modal-content'>"+
                 "<div class='modal-header'>"+
                     "<h1 class='display-5'>Carrito De Compra</h1>"+
@@ -144,7 +189,7 @@ function carrito(){
                     '</section>'+
             "</div>"+
         "</div>";
-}
+} */
 
 window.onload = carrito;
 
